a565f937459bdf60bcb557e4b4fc993c21c94a06
Sankalp verma <43778681+sanki4489@users.noreply.github.com>
2021-10-27 12:13:55 +0530

    Update iterators.js

--- iterators.js
+++ iterators.js
@@ -45,3 +45,25 @@ const smallNumbers = bigNumbers.map(number => {
 
 
 
+//The .filter() Method
+const randomNumbers = [375, 200, 3.14, 7, 13, 852];
+
+// Call .filter() on randomNumbers below
+const smallNumbers = randomNumbers.filter(number => {
+  return number < 250;
+})
+
+
+const favoriteWords = ['nostalgia', 'hyperbole', 'fervent', 'esoteric', 'serene'];
+
+
+// Call .filter() on favoriteWords below
+const longFavoriteWords = favoriteWords.filter(words => {
+  return words.length > 7;
+})
+
+
+
+
+
+//The .findIndex() Method
