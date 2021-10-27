df0bbd82cb399ee5e0360f43cf1ba7158e325601
Sankalp verma <43778681+sanki4489@users.noreply.github.com>
2021-10-27 17:02:56 +0530

    Create simpleObject.js

---
+++ simpleObject.js
@@ -0,0 +1,33 @@
+let spaceship = {
+  passengers: null,
+  telescope: {
+    yearBuilt: 2018,
+    model: "91031-XLT",
+    focalLength: 2032 
+  },
+  crew: {
+    captain: { 
+      name: 'Sandra', 
+      degree: 'Computer Engineering', 
+      encourageTeam() { console.log('We got this!') },
+     'favorite foods': ['cookies', 'cakes', 'candy', 'spinach'] }
+  },
+  engine: {
+    model: "Nimbus2000"
+  },
+  nanoelectronics: {
+    computer: {
+      terabytes: 100,
+      monitors: "HD"
+    },
+    'back-up': {
+      battery: "Lithium",
+      terabytes: 50
+    }
+  }
+};
+const capFave = spaceship.crew.captain['favorite foods'][0];
+  spaceship.passengers = [{name: 'Rose marie mar lo'}]
+const firstPassenger = spaceship.passengers[0]
+
+  
