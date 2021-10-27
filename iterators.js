dde48678ba1913f6b38896de26cf0ce2f2cc2b5c
Sankalp verma <43778681+sanki4489@users.noreply.github.com>
2021-10-27 10:56:32 +0530

    Update iterators.js

--- iterators.js
+++ iterators.js
@@ -1,3 +1,4 @@
+//for each
 const fruits = ['mango', 'papaya', 'pineapple', 'apple'];
 
 // Iterate over fruits below
@@ -14,3 +15,33 @@ function printFruits(fruit){
   console.log('I want to eat a ' + fruit);
 }
 fruits.forEach(printFruits)
+
+
+
+
+
+
+
+//.map()
+const animals = ['Hen', 'elephant', 'llama', 'leopard', 'ostrich', 'Whale', 'octopus', 'rabbit', 'lion', 'dog'];
+
+// Create the secretMessage array below
+const secretMessage = animals.map(animal => {
+  return animal[0];
+})
+
+
+console.log(secretMessage.join(''));
+
+const bigNumbers = [100, 200, 300, 400, 500];
+
+// Create the smallNumbers array below
+const smallNumbers = bigNumbers.map(number => {
+  return number / 100;
+})
+
+
+
+
+
+
