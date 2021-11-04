908f036227bfc2b5153647ec17e3405059560adf
Sankalp verma <43778681+sanki4489@users.noreply.github.com>
2021-11-04 10:55:11 +0530

    Update fizzbuzz.js

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,19 +1,4 @@
-// let numArray = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
-// function fizzBuzz(){
-//     for(let i=0; i<=numArray.length; i++){
-//         if(numArray[i] % 3 === 0){
-//             numArray[i] = "fizz" 
-//             alert(numArray[i])
-//         }
-//         else if(numArray[i] % 5 === 0){
-//             numArray[i] = "Buzz"
-//         }
-//         else if(numArray[i] % 3 === 0 && numArray[i] % 5 === 0){
-//             numArray[i] = "FizzBuzz"
-//         }
-//     }
-// }
-// alert(fizzBuzz())
+//without loop
 let arr = []
 let count = 1
 function fizzBuzz(){
@@ -31,4 +16,26 @@ function fizzBuzz(){
     }
     count++; 
     console.log(arr);
+}
+//with while loop
+
+let arr = []
+let count = 1
+function fizzBuzz(){
+  while(count<=30){  
+    if(count%3 === 0 && count%5 === 0){
+        arr.push("fizzbuzz");
+    }
+    else if(count%3 === 0){
+        arr.push("fizz");
+    }
+    else if(count%5 === 0){
+        arr.push("Buzz");
+    }
+    else{
+           arr.push(count);
+    }
+    count++; 
+    console.log(arr);
+  }  
 }
